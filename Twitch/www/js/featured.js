059bb0785effc075cbab48c41761e02ac5e50b19
Joey <joe>
2021-04-30 15:46:53 +0200

    quality of life update #1

---
+++ Twitch/www/js/featured.js
@@ -0,0 +1,66 @@
+
+$(".featured_nav").on("click", function () {
+
+const request_featured = new XMLHttpRequest(); //rq
+const client = "g7o1paeiwxkakqj4qs2zajdixk595n";
+const game_num = 6;
+let game_name = encodeURIComponent("League of Legends");
+
+//featured streams
+
+
+function featured_live(json) {
+  const live_container = document.querySelector(".featured_live");
+  live_container.innerHTML = "";
+  // console.log(json.streams.length);
+  console.log(json);
+  for (let i = 0; i < json.featured.length; i++) {
+    const game_tv = document.createElement("div"); //創建物件
+    game_tv.classList.add("color");
+    game_tv.setAttribute("data-stream", `${json.featured[i].stream.channel.display_name}`);
+    game_tv.setAttribute(
+      "style",
+      `background-image:url('${json.featured[i].stream.preview.large}');`
+    );
+
+    game_tv.innerHTML = `   
+     <div class="text-align_">
+
+     <img src="${json.featured[i].stream.channel.logo}"/>
+   <p class="viewers">${json.featured[i].stream.viewers}</p>
+   <div class="meta_data">
+   <p class="title">${json.featured[i].stream.channel.display_name}</p>
+   <p class="title_status">${json.featured[i].stream.channel.description}</p>
+   <p class="title_game">${json.featured[i].stream.game}</p>
+   </div>
+       
+        </div>
+     `;
+    live_container.appendChild(game_tv);
+  }
+}
+
+//GET HOME PAGE FOLLOWING
+$(document).ready(function () {
+  setTimeout(function () {
+   
+   
+    request_featured.onload = function () {
+      if (request_featured.status >= 200 && request_featured.status < 400) {
+        const response = request_featured.responseText;
+        const json = JSON.parse(response);
+
+        featured_live(json);
+
+        // console.log(json);
+      } else {
+        console.log("error");
+      }
+    };
+    request_featured.open("GET", "https://api.twitch.tv/kraken/streams/featured", true); //非同步
+    request_featured.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json");
+    request_featured.setRequestHeader("Client-ID", client);
+    request_featured.send();
+  });
+});
+});
--- Twitch/www/js/main.js
+++ Twitch/www/js/main.js
@@ -1,18 +1,110 @@
 // Get stream url
+
+// if(localStorage.getItem("mytime") === null){
+// alert("leeg")
+// }
+
+
 $(document).ready(function () {
   $(".username_update_input").val(`${localStorage.getItem("username")}`);
-  $(".settings_button").on("click", function () {
+
+
+
+
+  //set home nav auto active color
+  $(".home_nav").css("color","darkorchid");
+  $(".search_nav").css("color","#212529");
+  $(".featured_nav").css("color","#212529");
+  $(".settings_nav").css("color","#212529");
+
+  //set auto show div home
+  $(".videos_layout_container").show();
+  $(".search").hide();
+  $(".featured").hide();
+  $(".settings").hide();
+
+
+//home button_nav
+$(".home_nav").on("click", function () {
+  event.preventDefault();
+
+  //set color
+  $(".home_nav").css("color","darkorchid");
+  $(".search_nav").css("color","#212529");
+  $(".featured_nav").css("color","#212529");
+  $(".settings_nav").css("color","#212529");
+
+  
+  //set right div to show
+  $(".videos_layout_container").show();
+  $(".search").hide();
+  $(".featured").hide();
+  $(".settings").hide();
+});
+//end home button_nav
+
+//featured button_nav
+$(".featured_nav").on("click", function () {
+  event.preventDefault();
+
+  //set color
+  $(".home_nav").css("color","#212529");
+  $(".search_nav").css("color","#212529");
+  $(".featured_nav").css("color","darkorchid");
+  $(".settings_nav").css("color","#212529");
+
+  
+  //set right div to show
+  $(".videos_layout_container").hide();
+  $(".search").hide();
+  $(".featured").show();
+  $(".settings").hide();
+});
+//end featured button_nav
+
+//featured button_nav
+$(".search_nav").on("click", function () {
+  event.preventDefault();
+
+  //set color
+  $(".home_nav").css("color","#212529");
+  $(".search_nav").css("color","darkorchid");
+  $(".featured_nav").css("color","#212529");
+  $(".settings_nav").css("color","#212529");
+
+  
+  //set right div to show
+  $(".videos_layout_container").hide();
+  $(".search").show();
+  $(".featured").hide();
+  $(".settings").hide();
+});
+//end featured button_nav
+
+
+  //Settings button_nav
+  $(".settings_nav").on("click", function () {
     event.preventDefault();
+
+    //set color
+    $(".home_nav").css("color","#212529");
+    $(".search_nav").css("color","#212529");
+    $(".featured_nav").css("color","#212529");
+    $(".settings_nav").css("color","darkorchid");
+
+    
+    //set right div to show
     $(".videos_layout_container").hide();
+    $(".search").hide();
+    $(".featured").hide();
     $(".settings").show();
-    $(".navbar-toggler").click();
   });
+  //end Settings button_nav
 
   setTimeout(function () {
     $(".color").each(function () {
       var $this = $(this);
       $this.on("click", function () {
-        alert($(this).data("stream"));
         var streamname = $(this).data("stream");
         // redirect to node js website for stream m3u8 url
         $.get(
@@ -31,7 +123,7 @@ $(document).ready(function () {
         );
       });
     });
-  }, 500);
+  }, 900);
 });
 
 //
